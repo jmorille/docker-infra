0c599ca7cf0d3a85eb78a90e91499cb33e723a76
Morille Jerome <morille.jerome@groupagrica.com>
2016-08-19 11:50:31 +0200

    CA dir

--- cert/genCert.sh
+++ cert/genCert.sh
@@ -11,17 +11,16 @@ CERT_PASS="NPRn[X(9M(@82<>d7@*z"
 CERT_CN="localhost"
 
 
-
-
 CERT_FILENAME=domain
 NUMBITS=4096
 
 # Gen Path
 export TARGET_DIR=certs
-# export PUB_DIR=pub
-# export PRIV_DIR=priv
-export PUB_DIR=.
-export PRIV_DIR=.
+export CA_DIR=ca-certs
+export PUB_DIR=pub
+export PRIV_DIR=priv
+#export PUB_DIR=.
+#export PRIV_DIR=.
 
 
 # KeyStore
@@ -48,47 +47,51 @@ function docCertificateSubject {
 function createDestDir {
     mkdir -p $TARGET_DIR/$PRIV_DIR
     mkdir -p $TARGET_DIR/$PUB_DIR
+    mkdir -p $CA_DIR/$PRIV_DIR
+    mkdir -p $CA_DIR/$PUB_DIR
 }
   
 function createCA {
   # Créez une clé privée RSA pour votre serveur (elle sera au format PEM et chiffrée en Triple-DES) :
-  openssl  genrsa  -passout pass:$CA_PASS -des3 -out $PRIV_DIR/ca.key $NUMBITS
+  openssl  genrsa  -passout pass:$CA_PASS -des3 -out $CA_DIR/$PRIV_DIR/ca.key $NUMBITS
 
   #  Vous pouvez afficher les détails de cette clé privée RSA à l'aide de la commande :
-  # openssl rsa -noout -text -in $PRIV_DIR/ca.key
+  # openssl rsa -noout -text -in $CA_DIR/$PRIV_DIR/ca.key
 
   # Si nécessaire, vous pouvez aussi créer une version PEM non chiffrée (non recommandé) de cette clé privée RSA	avec :
-  # openssl rsa  -passin pass:$CA_PASS -in $PRIV_DIR/ca.key -out $PRIV_DIR/ca.key.unsecure
+  # openssl rsa  -passin pass:$CA_PASS -in $CA_DIR/$PRIV_DIR/ca.key -out $CA_DIR/$PRIV_DIR/ca.key.unsecure
 
   # Créez un certificat auto-signé (structure X509) à l'aide de la clé RSA que vous venez de générer (la sortie sera au format PEM) :
-  openssl req -passin pass:$CA_PASS -new -x509 -nodes -sha1 -days 365 -key $PRIV_DIR/ca.key -out $PUB_DIR/ca.crt -extensions usr_cert  -subj $CA_SUBJ
+  openssl req -passin pass:$CA_PASS -new -x509 -nodes -sha1 -days 365 -key $CA_DIR/$PRIV_DIR/ca.key -out $CA_DIR/$PUB_DIR/ca.crt -extensions usr_cert  -subj $CA_SUBJ
 
+  # Record password
+  echo $CA_PASS > $TARGET_DIR/$PRIV_DIR/ca-password.txt
   # printCA
 }
 
 function printCA {
    # Vous pouvez afficher les détails de ce certificat avec :
-   openssl x509 -passin pass:$CA_PASS -noout -text -in $PUB_DIR/ca.crt
+   openssl x509 -passin pass:$CA_PASS -noout -text -in $CA_DIR/$PUB_DIR/ca.crt
 }
 
 function createAutoSignCertificateTls {
  # create Auto Sign certificat  SSL fot test  
-  openssl req -new -x509 -nodes -out  $PRIV_DIR/$CERT_FILENAME.crt -keyout  $PRIV_DIR/$CERT_FILENAME.key -subj $CERT_SUBJ
+  openssl req -new -x509 -nodes -out  $TARGET_DIR/$PRIV_DIR/$CERT_FILENAME.crt -keyout  $TARGET_DIR/$PRIV_DIR/$CERT_FILENAME.key -subj $CERT_SUBJ
 }
 
 function createCertificateTls {
  echo "### Generate Certificate for Domain $CERT_CN"
  # Créez une clé privée RSA pour votre serveur Apache (elle sera au format PEM et chiffrée en Triple-DES):
- openssl genrsa -passout pass:$CERT_PASS -des3 -out $PRIV_DIR/$CERT_FILENAME.key $NUMBITS
+ openssl genrsa -passout pass:$CERT_PASS -des3 -out $TARGET_DIR/$PRIV_DIR/$CERT_FILENAME.key $NUMBITS
 
- # Enregistrez le fichier $PRIV_DIR/$CERT_FILENAME.key et le mot de passe éventuellement défini en lieu sûr.
+ # Enregistrez le fichier $TARGET_DIR/$PRIV_DIR/$CERT_FILENAME.key et le mot de passe éventuellement défini en lieu sûr.
  # TODO
 
  # Vous pouvez afficher les détails de cette clé privée RSA à l'aide de la commande :
- # openssl rsa -passin pass:$CERT_PASS -noout -text -in $PRIV_DIR/$CERT_FILENAME.key
+ # openssl rsa -passin pass:$CERT_PASS -noout -text -in $TARGET_DIR/$PRIV_DIR/$CERT_FILENAME.key
  
  # Créez une Demande de signature de Certificat (CSR) à l'aide de la clé privée précédemment générée (la sortie sera au format PEM):
- openssl req -passin pass:$CERT_PASS -new -key $PRIV_DIR/$CERT_FILENAME.key -out $PUB_DIR/$CERT_FILENAME.csr  -subj $CERT_SUBJ
+ openssl req -passin pass:$CERT_PASS -new -key $TARGET_DIR/$PRIV_DIR/$CERT_FILENAME.key -out $TARGET_DIR/$PUB_DIR/$CERT_FILENAME.csr  -subj $CERT_SUBJ
 
  # Vous devez entrer le Nom de Domaine Pleinement Qualifié ("Fully Qualified Domain Name" ou FQDN) 
  # de votre serveur lorsqu'OpenSSL vous demande le "CommonName", 
@@ -100,31 +103,31 @@ function createCertificateTls {
 
 function printCsr {
  # Vous pouvez afficher les détails de ce CSR avec :
- openssl req -passin pass:$CERT_PASS -noout -text -in $PUB_DIR/$CERT_FILENAME.csr
+ openssl req -passin pass:$CERT_PASS -noout -text -in $TARGET_DIR/$PUB_DIR/$CERT_FILENAME.csr
 }
 
 
 function signCertificateTlsWithCa {
   # La commande qui signe la demande de certificat est la suivante : ==>  CRT = CSR + CA sign
-  openssl x509 -passin pass:$CA_PASS -req -in $PUB_DIR/$CERT_FILENAME.csr -out $PUB_DIR/$CERT_FILENAME.crt -CA $PUB_DIR/ca.crt -CAkey $PRIV_DIR/ca.key -CAcreateserial -CAserial $PRIV_DIR/ca.srl
+  openssl x509 -passin pass:$CA_PASS -req -in $TARGET_DIR/$PUB_DIR/$CERT_FILENAME.csr -out $TARGET_DIR/$PUB_DIR/$CERT_FILENAME.crt -CA $CA_DIR/$PUB_DIR/ca.crt -CAkey $CA_DIR/$PRIV_DIR/ca.key -CAcreateserial -CAserial $CA_DIR/$PRIV_DIR/ca.srl
   
   # printCrt
 }
 
 function printCrt {
   # Une fois la CSR signée, vous pouvez afficher les détails du certificat comme suit :
-  openssl x509 -noout -text -in $PUB_DIR/$CERT_FILENAME.crt
+  openssl x509 -noout -text -in $TARGET_DIR/$PUB_DIR/$CERT_FILENAME.crt
 }
 
 function unpassswdCertificateTlsKey {
  # Supprimer le chiffrement de la clé privée RSA (tout en conservant une copie de sauvegarde du fichier original) :
- openssl rsa  -passin pass:$CERT_PASS  -in $PRIV_DIR/$CERT_FILENAME.key -out $PUB_DIR/$CERT_FILENAME-nopasswd.key
+ openssl rsa  -passin pass:$CERT_PASS  -in $TARGET_DIR/$PRIV_DIR/$CERT_FILENAME.key -out $TARGET_DIR/$PUB_DIR/$CERT_FILENAME-nopasswd.key
 }
 
 function mergeAllCertificats {
- # -->  $PUB_DIR/allcacerts.crt
- # combines the cacerts file from the openssl distribution $PUB_DIR/allcacerts.crt and the intermediate.crt file.
- cat $PUB_DIR/$CERT_FILENAME.crt $PUB_DIR/ca.crt > $PUB_DIR/allcacerts.crt
+ # -->  $TARGET_DIR/$PUB_DIR/allcacerts.crt
+ # combines the cacerts file from the openssl distribution $TARGET_DIR/$PUB_DIR/allcacerts.crt and the intermediate.crt file.
+ cat $TARGET_DIR/$PUB_DIR/$CERT_FILENAME.crt $CA_DIR/$PUB_DIR/ca.crt > $TARGET_DIR/$PUB_DIR/allcacerts.crt
  # Convert crt to pem
  # openssl x509 -in allcacerts.crt -out allcacerts.pem -outform PEM
  # openssl x509 -inform DER -in allcacerts.crt -out allcacerts.pem -text
@@ -142,7 +145,7 @@ function createNewKeystorePKCS12 {
    fi
 
    # Test for all Certificate File
-   FILE_ALL_CERT=$PUB_DIR/allcacerts.crt
+   FILE_ALL_CERT=$TARGET_DIR/$PUB_DIR/allcacerts.crt
    if [ -f "$FILE_ALL_CERT" ]
    then
 	echo "$FILE_ALL_CERT found." 
@@ -152,7 +155,7 @@ function createNewKeystorePKCS12 {
    fi
 
    # create PKCS12 format keystores.
-   openssl pkcs12 -passin pass:$CERT_PASS -passout pass:$KEYSTORE_PK12_PASS  -export -in $PUB_DIR/$CERT_FILENAME.crt -inkey $PRIV_DIR/$CERT_FILENAME.key -out $FILE_KEYSTORE_PKCS12 -name tomcat -CAfile $PUB_DIR/allcacerts.crt -caname root -chain
+   openssl pkcs12 -passin pass:$CERT_PASS -passout pass:$KEYSTORE_PK12_PASS  -export -in $TARGET_DIR/$PUB_DIR/$CERT_FILENAME.crt -inkey $TARGET_DIR/$PRIV_DIR/$CERT_FILENAME.key -out $FILE_KEYSTORE_PKCS12 -name tomcat -CAfile $TARGET_DIR/$PUB_DIR/allcacerts.crt -caname root -chain
  }
 
 function createNewKeystoreJKS {
@@ -165,7 +168,7 @@ function createNewKeystoreJKS {
 	echo "$FILE_KEYSTORE_JKS not found."
    fi
 
-   #keytool -certreq -keyalg RSA -alias server -file $PUB_DIR/$CERT_FILENAME.csr -keystore server-keystore.jks -storepass $CA_PASS01 -keypass $CA_PASS01
+   #keytool -certreq -keyalg RSA -alias server -file $TARGET_DIR/$PUB_DIR/$CERT_FILENAME.csr -keystore server-keystore.jks -storepass $CA_PASS01 -keypass $CA_PASS01
    # keytool -genkey -alias server -keyalg rsa -keysize 1024 -keystore server-keystore.jks -storetype JKS -storepass $CA_PASS01 -keypass $CA_PASS01 -dname "CN=integ2, OU=COM, O=$CA_PASS, L=PARIS, ST=PARIS, C=FR, emailAddress=test@$CA_PASS.fr" 
 
    keytool -genkey -alias tomcat -keyalg RSA  -keysize $NUMBITS -keypass $CERT_PASS -keystore $FILE_KEYSTORE_JKS -storetype JKS -storepass $KEYSTORE_JKS_PASS -dname "CN=integ2, OU=COM, O=Organisation, L=PARIS, ST=PARIS, C=FR, emailAddress=test@organisation.fr"
@@ -173,10 +176,10 @@ function createNewKeystoreJKS {
 
 function importKeystoreJKSCertificates { 
  # Import the Chain Certificate into your keystore   
- # keytool -import -alias root -keystore $FILE_KEYSTORE_JKS -storepass $KEYSTORE_JKS_PASS -trustcacerts -noprompt -file  $PUB_DIR/allcacerts.crt
+ # keytool -import -alias root -keystore $FILE_KEYSTORE_JKS -storepass $KEYSTORE_JKS_PASS -trustcacerts -noprompt -file  $TARGET_DIR/$PUB_DIR/allcacerts.crt
 
- # keytool -import -alias root -keystore $FILE_KEYSTORE_JKS -storepass $KEYSTORE_JKS_PASS -trustcacerts -noprompt -file  $PUB_DIR/ca.crt
- # keytool -import -alias tomcat -keystore $FILE_KEYSTORE_JKS -storepass $KEYSTORE_JKS_PASS -trustcacerts -noprompt -file  $PUB_DIR/$CERT_FILENAME.crt
+ # keytool -import -alias root -keystore $FILE_KEYSTORE_JKS -storepass $KEYSTORE_JKS_PASS -trustcacerts -noprompt -file  $CA_DIR/$PUB_DIR/ca.crt
+ # keytool -import -alias tomcat -keystore $FILE_KEYSTORE_JKS -storepass $KEYSTORE_JKS_PASS -trustcacerts -noprompt -file  $TARGET_DIR/$PUB_DIR/$CERT_FILENAME.crt
  # keytool -alias dmzlan -import -keystore /DATA/API/certificates/api_keystore.jks -file /DATA/API/certificates/server-lanDmz.crt -storepass $KEYSTORE_JKS_PASS
  
  # List KeyStore
@@ -190,7 +193,7 @@ function printKeystoreJKSCertificates {
 
 function createDHECertificate {
   cd  ssl
-  openssl dhparam -out $PUB_DIR/dhparam.pem $NUMBITS
+  openssl dhparam -out $TARGET_DIR/$PUB_DIR/dhparam.pem $NUMBITS
 }
 
 function setup {
@@ -199,19 +202,19 @@ function setup {
   # chmod +x /build/*.sh
 
 
-  # Create Ca Certificate ==> $PUB_DIR/ca.crt 
+  # Create Ca Certificate ==> $CA_DIR/$PUB_DIR/ca.crt 
   createCA|| exit 1
 
-  # Create Server Certificate ==>  $PUB_DIR/$CERT_FILENAME.csr
+  # Create Server Certificate ==>  $TARGET_DIR/$PUB_DIR/$CERT_FILENAME.csr
   createCertificateTls || exit 1
 
-  # Sign Server Certificate With CA  ==> $PUB_DIR/$CERT_FILENAME.crt
+  # Sign Server Certificate With CA  ==> $TARGET_DIR/$PUB_DIR/$CERT_FILENAME.crt
   signCertificateTlsWithCa || exit 1
 
   # Supprimer le chiffrement de la clé privée RSA  : ==> server-nopasswd.key
   unpassswdCertificateTlsKey || exit 1
   
-  # Merge All Certificats ==> $PUB_DIR/allcacerts.crt
+  # Merge All Certificats ==> $TARGET_DIR/$PUB_DIR/allcacerts.crt
   mergeAllCertificats || exit 1
   
   # KeyStore PKCS12 ==> $CERT_FILENAME.p12
@@ -359,7 +362,7 @@ echo "# ########################################## #"
 echo ""
 
 createDestDir
-cd $TARGET_DIR
+#cd $TARGET_DIR
 
 
 case "${!OPTIND-}" in
